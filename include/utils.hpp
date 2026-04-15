c4474d2afb8884f7264f1012dd7bcc686eb5b865
rogovogor17 <tolik_rogov@bk.ru>
2026-04-15 18:16:00 +0300

    start

---
+++ include/utils.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file     utils.hpp
+ * @brief   Some helpful functions
+ * @author  Rogov Anatoliy
+ * @date	2026-03-17
+ *
+ * @details
+ *
+ * Usage Example:
+ * @code{.cpp}
+ * @endcode
+ */
+
+#pragma once
+
+int add(int a, int b);
+int mul(int a, int b);
