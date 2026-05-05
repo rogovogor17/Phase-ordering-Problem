650ec0af8da94091046264a09bcc35dfdfe17d4c
rogovogor17 <tolik_rogov@bk.ru>
2026-05-05 16:56:44 +0300

    phase ordering schema

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,17 @@
 #include "utils.hpp"
 
+/**
+ * @brief Adding two integers
+ *
+ * @startuml add
+ * !include _theme_light.puml
+ * rectangle Adding as ad
+ * rectangle First as f
+ * rectangle Second as s
+ * ad -> f
+ * ad -> s
+ * @enduml
+ */
 int add(int a, int b) { return a + b; }
 
 int mul(int a, int b) { return a * b; }
